2202b80d6caec4408400607f5e3de0de859bb962
csabagyorfi <csaba@madetech.com>
2019-02-17 17:49:55 +0000

    messy solution

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -1,6 +1,6 @@
 class Number_to_lcd
   LCD_NUMBERS =
-    { '1' => "  \n |\n |\n",
+    { '1' => "   \n | \n | \n",
       '2' => " _ \n _|\n|_ \n",
       '3' => " _ \n _|\n _|\n",
       '4' => "   \n|_|\n  |\n",
@@ -11,13 +11,45 @@ class Number_to_lcd
       '9' => " _ \n|_|\n _|\n",
       '0' => " _ \n| |\n|_|\n" }.freeze
 
-  def self.display(integer)
+  def self.display(integer, size=1)
     result = ''
     numbers_as_string_array = integer.to_s.chars
-    (0..2).each do |index|
-      numbers_as_string_array.each { |number| result << LCD_NUMBERS[number].split("\n")[index] }
+
+    resised_numbers_array = []
+    numbers_as_string_array.each{|number| resised_numbers_array << resize(LCD_NUMBERS[number], size) }
+
+    height_of_digits = resised_numbers_array[0].count("\n") - 1
+    
+    (0..height_of_digits).each do |index|
+      resised_numbers_array.each { |number| result << number.split("\n")[index] }
       result << "\n"
     end
+
     result
+  end
+
+  def self.resize(lcd_number, size)
+      return lcd_number if size == 1
+      resized_lcd_number = ''
+
+      lcd_copy = lcd_number.gsub '_', '_' + '_' * size
+      lcd_copy.gsub! ' |', ' ' * size + ' |'
+      lcd_to_lines = lcd_copy.split("\n")
+
+      lcd_to_lines.each do |line|
+        if line.include?('|')
+          line_with_pipes = line.gsub '_', ' '
+          line_with_underscores = line.gsub '|', ' '
+          size.times {resized_lcd_number << line_with_pipes + "\n"}
+          resized_lcd_number << line_with_underscores + "\n"
+        else
+          if line.include?('_')
+            resized_lcd_number << line + "\n"
+          else
+            resized_lcd_number << line + (' ' * size) + "\n"
+          end
+        end
+      end
+    resized_lcd_number
   end
 end
--- spec/number_to_lcd_spec.rb
+++ spec/number_to_lcd_spec.rb
@@ -4,7 +4,7 @@ require 'number_to_lcd'
 describe Number_to_lcd do
   describe '#display' do
     it 'converts number 1 to "  \n |\n |\n"' do
-      expect(Number_to_lcd.display(1)).to eq("  \n |\n |\n")
+      expect(Number_to_lcd.display(1)).to eq("   \n | \n | \n")
     end
 
     it 'converts number 2 to " _ \n _|\n|_ \n"' do
@@ -20,11 +20,32 @@ describe Number_to_lcd do
     end
 
     it 'converts number 10 to " _ \n|_|\n _|\n"' do
-      expect(Number_to_lcd.display(10)).to eq("   _ \n || |\n ||_|\n")
+      expect(Number_to_lcd.display(10)).to eq("    _ \n | | |\n | |_|\n")
     end
 
     it 'converts number 123456789 to " _ \n|_|\n _|\n"' do
-      expect(Number_to_lcd.display(123_456_789)).to eq("   _  _     _  _  _  _  _ \n | _| _||_||_ |_   ||_||_|\n ||_  _|  | _||_|  ||_| _|\n")
+      expect(Number_to_lcd.display(123456789)).to eq("    _  _     _  _  _  _  _ \n |  _| _||_||_ |_   ||_||_|\n | |_  _|  | _||_|  ||_| _|\n")
     end
+
+    it 'can convert single digits numbers to double size' do
+      expect(Number_to_lcd.display(2, size = 2)).to eq(" ___ \n    |\n    |\n ___ \n|    \n|    \n ___ \n")
+    end
+
+    it 'can convert single digits numbers to double size again' do
+      expect(Number_to_lcd.display(3, size = 2)).to eq(" ___ \n    |\n    |\n ___ \n    |\n    |\n ___ \n")
+    end
+
+    it 'can convert single digits 1 to any size double size, single number 2' do
+      expect(Number_to_lcd.display(1, size = 3)).to eq("      \n    | \n    | \n    | \n      \n    | \n    | \n    | \n      \n")
+    end
+
+    it 'can convert single digits 0 to any size, single number 2' do
+      expect(Number_to_lcd.display(0, size = 3)).to eq(" ____ \n|    |\n|    |\n|    |\n      \n|    |\n|    |\n|    |\n ____ \n")
+    end
+
+    it 'can convert multiple digits to any size' do
+      expect(Number_to_lcd.display(12345678910, size = 4)).to eq("")
+    end
+    #0, 4, 7,
   end
 end
